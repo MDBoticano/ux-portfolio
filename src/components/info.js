05e620073bd4f03924307157e5e615e1032bdd7b
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-23 14:50:01 -0700

    Modified information centering

--- src/components/info.js
+++ src/components/info.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div id="info-box">
-      <div className="verticalSpacer"></div>
+    <div id="info-container">
+      {/* <div className="verticalSpacer"></div> */}
       <div className="info">
-        <h1 id="name">Marlo Boticano</h1>
-        <h2 id="email">m.boticano@gmail.com</h2>
+        <h1 id="text-name">Marlo&nbsp;Boticano</h1>
+        <h2 id="text-email">m.boticano@gmail.com</h2>
       </div>
-      <div className="verticalSpacer"></div>
+      {/* <div className="verticalSpacer"></div> */}
     </div>
   )
 }
