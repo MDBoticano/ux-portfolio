63a1a5b76ffa48c4b67db586ad4f8499d2d25490
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-29 15:49:25 -0700

    Tweaks to make UX portfolio

--- src/pages/contents.js
+++ src/pages/contents.js
@@ -4,41 +4,22 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const TableOfContents = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the "secret" table of contents</h1>
     <p>Welcome!</p>
     <ul>
       Portfolio Pages
-      <li><Link to="/">Go back to the homepage</Link></li>
-      <li><Link to="/error">Go to Error page</Link></li>
+      <li><Link to="/">Homepage</Link></li>
       <li><Link to="/projects/test-md">Markdown Page 1</Link></li>
     </ul>
-    <ul>
-      React Project Pages
-      <li>
-        <a href="https://mdboticano.github.io/frank-ocean-lyrics">
-          Frank Lyrics
-        </a>
-      </li>
-      <li>
-        <a href="https://mdboticano.github.io/markdown-previewer">
-          Markdown Previewer
-        </a>
-      </li>
-      <li>
-        <a href="https://mdboticano.github.io/pomodoro">
-          Pomodoro
-        </a>
-      </li>
-      <li>
-        <a href="https://mdboticano.github.io/react-calculator">
-          Calculator
-        </a>
-      </li>
-    </ul>
+    <p>Interested in my development work? Checkout my&nbsp;
+    <a href="https://mdboticano.github.io">
+      dev portfolio
+    </a>
+    &nbsp;instead</p>
   </Layout>
 )
 
-export default SecondPage
+export default TableOfContents
