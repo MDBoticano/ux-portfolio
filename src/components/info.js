d524c740032c96296a226aa234b8ecaa2fdf03de
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-29 16:07:39 -0700

    Removed description (was only used for testing)

--- src/components/info.js
+++ src/components/info.js
@@ -4,7 +4,6 @@ const Info = () => {
   return (
     <div className="info">
       <h1 id="text-name">Marlo&nbsp;Boticano</h1>
-      <h2 id="text-title">UX Researcher</h2>
       <h2 id="text-email">m.boticano@gmail.com</h2>
     </div>
   )
