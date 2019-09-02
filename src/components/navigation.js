77251ddc295232a03977a2d2297d5f01a38fb025
MDBoticano <MDBoticano@users.noreply.github.com>
2019-09-02 15:55:24 -0700

    Tweaks to try to use images in Markdown/mdx

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 import LogoImage from './logo'
 
-const Navigation = () => {
-  const LinkToResume = "https://drive.google.com/file/d/1zILQT_FgdzYh6saWl1oBFD9sK32U0Uh_/view"
+const LinkToResume = "https://drive.google.com/file/d/1zILQT_FgdzYh6saWl1oBFD9sK32U0Uh_/view"
 
+const Navigation = () => {
   return (
     <nav className="navigation" aria-label="navigation">
       <LogoImage />
