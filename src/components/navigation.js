a9ad2231ccd1e7e2039a72875bc4497774d69d34
MDBoticano <MDBoticano@users.noreply.github.com>
2019-09-02 12:38:23 -0700

    Made resume link a variable

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,15 +3,15 @@ import { Link } from 'gatsby'
 import LogoImage from './logo'
 
 const Navigation = () => {
+  const LinkToResume = "https://drive.google.com/file/d/1zILQT_FgdzYh6saWl1oBFD9sK32U0Uh_/view"
+
   return (
     <nav className="navigation" aria-label="navigation">
       <LogoImage />
       <div className="links">
         <Link to="/">Work</Link>
         <Link to="/about">About</Link>
-        <a href="https://drive.google.com/open?id=1zILQT_FgdzYh6saWl1oBFD9sK32U0Uh_"
-          rel="noopener noreferrer"
-          target="_blank">
+        <a href={LinkToResume} rel="noopener noreferrer" target="_blank">
           Resume
         </a>
       </div>
