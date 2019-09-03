00f286774c1f5f7666f2867726e2d7a773c35223
Marlo Boticano <m.boticano@gmail.com>
2019-09-03 11:18:36 -0700

    Content: organized table of contents

--- src/pages/contents.js
+++ src/pages/contents.js
@@ -8,22 +8,22 @@ const TableOfContents = () => (
   <>
     <SEO title="Contents" />
     <div className="table-of-contents">
-      <h1>Hi from the "secret" table of contents</h1>
+      <h1>Hello from the table of contents!</h1>
       <p>Welcome!</p>
       <p>Nav Pages</p>
       <ul>        
         <li><Link to="/">Homepage</Link></li>
         <li><Link to="/about">About Me</Link></li>
-        <li><Link to="/about/me">About / Me</Link></li>
         <li><Link to="/404">404</Link></li>
-        <li><Link to="/405">405</Link></li>
       </ul>
 
       <p>Project Pages</p>
       <ul>        
         <li><Link to="/projects/feed-the-kids">Feed the Kids</Link></li>
+        <li><Link to="/projects/matchmaker">Matchmaker</Link></li>
+        <li><Link to="/projects/uc-socially-driven">UC Socially Driven</Link></li>
+        <li><Link to="/projects/oilgo">OilGo</Link></li>
       </ul>
-
 
       <p>Interested in my development work? Checkout my&nbsp;
         <a href="https://mdboticano.github.io">
