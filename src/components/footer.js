8dd036de14f36ae540412ae5159d2b6571564c5d
mdboticano <mdboticano@gmail.com>
2019-09-04 16:33:31 -0700

    Styling: homepage tweaks to balance images with description

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,21 @@ import { Link } from 'gatsby'
 const Footer = () => {
   return (
     <div className="footer">
-      <Link to="/contents" className="link-to-contents">Contents</Link>
+      <p className="footer-email">m.boticano@gmail.com</p>
+      <ul className="footer-contacts">
+        <li className="footer-linkedin">
+          <a href="https://www.linkedin.com/in/marlo-daniel-boticano">
+            LinkedIn
+          </a>
+        </li>
+        <li className="footer-github">
+          <a href="https://github.com/MDBoticano">GitHub</a>
+        </li>
+        <li className="footer-twitter">
+          <a href="https://twitter.com/marloboticano">Twitter</a>
+        </li>
+      </ul>
+      
     </div>
   )
 }
