c9570c818b89862802f72c4b7e2fb4d05a87167c
mdboticano <mdboticano@gmail.com>
2019-09-04 15:14:44 -0700

    Content & Styling: Homepage project layout and images (Matchmaker still missing image)

--- src/components/info.js
+++ src/components/info.js
@@ -9,67 +9,75 @@ const Info = () => {
     query {
       FTK: file(relativePath: { eq: "feed-the-kids/ftk_thumbnail.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 500) {
             ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
       MM: file(relativePath: { eq: "matchmaker/mm_thumbnail.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 500) {
             ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
       OG: file(relativePath: { eq: "oilgo/og_thumbnail.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 500) {
             ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
-      SD: file(relativePath: { eq: "uc-socially-driven/ucsd_thumbnail.png" }) {
+      SD: file(relativePath: { eq: "uc-socially-driven/sd_thumbnail.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 500) {
             ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
     }
   `)
+  
+  /* Turn query results into Gatsby Image, then pass as prop */
+  const ftkImage = <Img fluid={ProjectThumbnails.FTK.childImageSharp.fluid}/>
+  const mmImage = <Img fluid={ProjectThumbnails.MM.childImageSharp.fluid}/>
+  const ogImage = <Img fluid={ProjectThumbnails.OG.childImageSharp.fluid}/>
+  const sdImage = <Img fluid={ProjectThumbnails.SD.childImageSharp.fluid}/>
 
   return (
     <div className="info-container">
     <div className="info">
       <p className="mb-name">Marlo&nbsp;Boticano</p>
       <p className="mb-title">User Researcher</p>
+      
       <ul className="projects-grid">
+      <Project 
+        title='Feed the Kids'
+        link='/projects/feed-the-kids'
+        description='How might we help a non-profit fight food insecurity more efficiently?'
+        image={ftkImage}
+      />
 
-        <Project title='Feed the Kids' link='/projects/feed-the-kids'
-          description='How might we help a non-profit fight food insecurity more efficiently?'
-          imgage={ProjectThumbnails.FTK.childImageSharp.fluid}
-        >
-          <Img fluid={ProjectThumbnails.FTK.childImageSharp.fluid}/>
-        </Project>
+      <Project 
+        title='Matchmaker' 
+        link='/projects/matchmaker'
+        description='How might we better bridge the gap from graduation to employment?'
+        image={mmImage}
+      />
 
-        <Project title='Matchmaker' link='/projects/matchmaker'
-          description='How might we better bridge the gap from graduation to employment?'
-        >
-          <Img fluid={ProjectThumbnails.MM.childImageSharp.fluid}/>
-        </Project>
+      <Project
+        title='OilGo'
+        link='/projects/oilgo'
+        description='How might we reduce the frustrations of car maintenance?'
+        image={ogImage}
+      />
 
-        <Project title='OilGo' link='/projects/oilgo'
-          description='How might we reduce the frustrations of car maintenance?'
-        >
-          <Img fluid={ProjectThumbnails.OG.childImageSharp.fluid}/>
-        </Project>
-
-        <Project title='UC Socially Driven' link='/projects/uc-socially-driven'
-          description='How might we encourage social activities that foster a better sense of community?'
-        >
-          <Img fluid={ProjectThumbnails.OG.childImageSharp.fluid}/>
-        </Project>
-        
+      <Project
+        title='UC Socially Driven'
+        link='/projects/uc-socially-driven'
+        description='How might we encourage social activities that foster a better sense of community?'
+        image={sdImage}
+      />
       </ul>
     </div>
     </div>
--- src/components/project.js
+++ src/components/project.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import ProjectPreview from './projectPreview'
 
-const Project = ({ title, description, link, children }) => {
+const Project = ({ title, description, link, image }) => {
   return (
     <li className = "project-row">
       <div className = "project-image">
         <Link to = { link }>
-          {children}
+          {image}
         </Link>
 
       </div>
