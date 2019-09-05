8b7cacc86ed0eea2bc5b563fc63ac50da7ff96f8
mdboticano <mdboticano@gmail.com>
2019-09-04 17:21:19 -0700

    Updates

---
+++ component---src-pages-404-js-33592b67d4ed39165ee7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{226:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(50),i=a(223);t.default=function(){return o.a.createElement("div",{className:"mb-content-404"},o.a.createElement(i.a,{title:"404: Not Found"}),o.a.createElement("div",{className:"page-not-found"},o.a.createElement("p",{id:"big-404"},"404"),o.a.createElement("h1",{id:"description-404"},"PAGE NOT FOUND"),o.a.createElement(c.a,{to:"/",id:"go-home"},"Return to Homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-33592b67d4ed39165ee7.js.map
