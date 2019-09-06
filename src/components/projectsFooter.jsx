8995d1d62eed9bd26191dd337092b72d3db95c20
MDBoticano <MDBoticano@users.noreply.github.com>
2019-09-06 11:30:46 -0700

    Content: Created project footer to navigate projects at the bottom of the page

---
+++ src/components/projectsFooter.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const ProjectsFooter = ({ active }) => {
+  const projectsList = [
+    {
+      title: 'All',
+      path: '/'
+    },
+    { 
+      title: 'Feed the Kids',
+      path: '/projects/feed-the-kids'
+    },
+    { 
+      title: 'Matchmaker',
+      path: '/projects/matchmaker'
+    },
+    { 
+      title: 'OilGo',
+      path: '/projects/oilgo'
+    },
+    { 
+      title: 'UC Socially Driven',
+      path: '/projects/uc-socially-driven'
+    },
+
+  ]
+
+  const isProjectActive = (project, active) => {
+    if (project.title === active) {
+      return "active-project-link"
+    } else {
+      return ""
+    }
+  }
+
+  const listProjects = ( projects ) => {
+    const projectLinks = projects.map( (project) => {
+     
+      return (
+      <Link 
+        to={project.path} 
+        className={`footer-link ${isProjectActive(project, active)}`}
+        key={project.title}
+      >
+        {project.title}
+      </Link>)
+    })
+
+    return projectLinks
+  }
+
+  const footerStyle = {
+    marginTop: 0,
+    marginBottom: '0.125rem',
+    fontWeight: 'bold',
+  }
+
+
+  return (
+    <div className="projects-footer">
+      <p className="projects-footer-description" style={footerStyle}>
+        Projects
+      </p>
+      <div className="projects-list-links">
+        {listProjects(projectsList)}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectsFooter
