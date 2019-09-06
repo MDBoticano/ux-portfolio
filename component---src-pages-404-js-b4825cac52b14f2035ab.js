edf429977cf92a4c4aaae3a3eab0c3476ebf8878
MDBoticano <MDBoticano@users.noreply.github.com>
2019-09-06 12:13:37 -0700

    Updates

---
+++ component---src-pages-404-js-b4825cac52b14f2035ab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{229:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),c=a(39),i=a(226);t.default=function(){return o.a.createElement("div",{className:"mb-content-404"},o.a.createElement(i.a,{title:"404: Not Found"}),o.a.createElement("div",{className:"page-not-found"},o.a.createElement("p",{id:"big-404"},"404"),o.a.createElement("h1",{id:"description-404"},"PAGE NOT FOUND"),o.a.createElement(c.a,{to:"/",id:"go-home"},"Return to Homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b4825cac52b14f2035ab.js.map
